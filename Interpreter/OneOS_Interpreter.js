72db2c1e395092761c35b29f3312d8e8ca4deef2
Brennan <brennancathcart@gmail.com>
2021-04-01 15:05:12 -0700

    latest interpreter code (not working)

--- Interpreter/OneOS_Interpreter.js
+++ Interpreter/OneOS_Interpreter.js
@@ -121,10 +121,10 @@ function TokenStream(input) {
         };
     }
 
-    // Note: We're not currently interpreting escape characters (/n, /t, etc.)
+    // Note: We're not currently interpreting escape characters (\n, \t, etc.)
     function read_escaped(end) {
-        var escaped = false,
-            str = "";
+        var escaped = false;
+        var str = "";
         input.next();
         while (!input.eof()) {
             var ch = input.next();
@@ -362,7 +362,7 @@ function parse(input) {
             type: "cmd",
             cmd: "connect",
             args: [parsedList],
-            name: name,
+            graph: name,
         };
     }
 
@@ -370,6 +370,11 @@ function parse(input) {
         cmd = input.next().value;
 
         var args = parse_args();
+        if (args.length == 0) {
+            input.croak(
+                `\"${cmd}\" command expects a *.js or *.py file as an argument`
+            );
+        }
 
         // Parse attributes
         var attrs = [];
@@ -385,19 +390,20 @@ function parse(input) {
             type: "cmd",
             cmd: cmd,
             args: args,
-            name: name,
+            group: name,
             attrs: attrs,
         };
     }
 
     function parse_cmd() {
-        skip_cmd();
+        cmd = input.next().value;
 
+        // TODO: suport unix style options?
         var args = parse_args();
 
         return {
             type: "cmd",
-            cmd: cmd.value,
+            cmd: cmd,
             args: args,
         };
     }
@@ -405,8 +411,7 @@ function parse(input) {
     function parse_args() {
         var args = [];
         while (is_valid_arg()) {
-            var arg = input.next();
-            args.push(arg.value);
+            args.push(input.next());
         }
         return args;
     }
@@ -465,7 +470,7 @@ function parse(input) {
             tok = input.next();
             return {
                 type: "id",
-                name: tok.value,
+                value: tok.value,
             };
         }
 
@@ -494,8 +499,252 @@ function parse(input) {
     }
 }
 
+function Edge(sender, receiver, pipe) {
+    this.sender = sender;
+    this.receiver = receiver;
+    this.pipe = pipe;
+}
+
+function Graph(name, edges) {
+    this.name = name;
+    this.edges = edges;
+}
+
+function Node(name, spawned, script, args, attrs) {
+    this.name = name;
+    this.spawned = spawned;
+    this.script = script;
+    this.args = args;
+    this.attrs = attrs;
+}
+
+// Maps name to a graph
+var Graphs = {};
+
+// Maps name to a list of one or more nodes (staged or spawned)
+var NodeGroups = {};
+
+var spawnQueue = [];
+
+// Can have unamed processes
+// - keep a list of all processes?
+
+function is_node_group(name) {
+    return NodeGroups[name] != undefined;
+}
+
+function is_graph(name) {
+    return Graphs[name] != undefined;
+}
+
+function is_piping_op(exp) {
+    var piping_ops = " * -> -*> ~> ~/> ~*> ";
+    return exp.type == "op" && piping_ops.includes(exp.operator) >= 0;
+}
+
+function spawn_node_group(name) {
+    // TODO: Likely call something instead
+    // var node_group = NodeGroups[name];
+    // groups_to_spawn.push(node_group);
+}
+
+function spawn_graph(name) {
+    // TODO: Likely call something instead
+    // var graph = Graphs[name];
+    // graphs_to_spawn.push(graph);
+}
+
+function generate_node_name(file_name) {
+    // TODO: ask Kumseok how to identify files
+    return file_name + Math.floor(Math.random() * 100);
+}
+
+function push_node_group(group, nd) {
+    if (NodeGroups[group] == undefined) {
+        NodeGroups[group] = [];
+    }
+    NodeGroups[group].push(nd);
+}
+
+function verify_node_arg(cmd, first_arg) {
+    var arg_val = first_arg.value;
+    if (first_arg.type == "num") {
+        throw new Error(
+            `Invalid first argument \"${arg_val}\": \"${cmd}\" command needs a *.js or *.py file as the first argument`
+        );
+    }
+
+    var file_types = " .js  .py ";
+    if (file_types.indexOf(arg_val.substr(arg_val.length - 3)) < 0) {
+        throw new Error(
+            `Invalid file \"${arg_val}\": \"${cmd}\" command needs a *.js or *.py file as the first argument`
+        );
+    } else {
+        // TODO: check the file exists
+    }
+}
+
+function create_node(exp, spawn) {
+    var file_name = exp.args[0];
+    verify_node_arg(cmd, file_name);
+
+    var name = generate_node_name(file_name); // TODO: a name may not be needed
+
+    var nd = new Node(name, spawn, file_name, exp.args.slice(1), exp.attrs);
+    if (exp.group) {
+        push_node_group(exp.group, nd);
+    } else if (spawn == false) {
+        throw new Error("A staged node must be assigned to a node group");
+    }
+
+    if (spawn == true) {
+        spawnQueue.push(nd);
+    }
+
+    return nd;
+}
+
+function evaluate(exp) {
+    switch (exp.type) {
+        case "num":
+        case "str":
+        case "id":
+            return exp.value;
+
+        case "cmd":
+            return evaluate_cmd(exp);
+
+        case "op":
+            return apply_op(exp.operator, exp.left, exp.right);
+
+        case "list":
+            var val = [];
+            for (var exp of exp.elems) {
+                val.push(evaluate(exp));
+            }
+            return val;
+
+        case "prog":
+            exp.prog.forEach(function (exp) {
+                console.log(evaluate(exp)); // TODO: remove print after debugging
+            });
+            return;
+
+        default:
+            throw new Error("I don't know how to evaluate " + exp.type);
+    }
+}
+
+function evaluate_cmd(exp) {
+    var cmd = exp.cmd;
+    switch (cmd) {
+        case "ls":
+        case "ps":
+        case "cd":
+        case "pwd":
+        case "cat":
+            console.log("UNIX COMMAND: " + cmd);
+            return;
+
+        case "node":
+            // Automatically assign it to a group as <script> if no name specified?
+            // Must have one argument that's a JS or python file
+            // -check that the file exists
+            return create_node(exp, false);
+
+        case "spawn":
+            // Spawn can be followed by either a single identifier (of a node group or graph)
+            // or some arguments
+            // Automatically assign a name as <script>.# if no name specified
+
+            // If first arg is a string, then we are spawning an existing node group or graph
+            var first_arg = exp.args[0];
+            if (first_arg.type == "str") {
+                // Get the identifier
+                if (is_node_group(first_arg.value)) {
+                    spawn_node_group(first_arg.value);
+                } else if (is_graph(first_arg.value)) {
+                    spawn_graph(first_arg.value);
+                } else {
+                    throw new Error(
+                        `\"${first_arg}\" does not correspond to a node group or graph`
+                    );
+                }
+            } else {
+                // We are spawning a process
+                return create_node(exp, true);
+            }
+
+        // Return reference to node-group or graph?
+
+        case "connect":
+            // Create a graph
+            // Must have a name
+            // Each entry in the list should be an operation
+
+            // TODO: ask Kumseok what format the internal graph structure should have
+
+            var graph_name = exp.graph;
+            if (!graph_name) {
+                throw new Error("A graph must have a name");
+            } else if (is_graph(graph_name)) {
+                throw new Error(`Graph \"${graph_name}\" already exists`);
+            }
+
+            var edges = evaluate(exp.args[0]);
+            console.log(edges);
+
+            // var edgeList = exp.args[0];
+            // for (var edge of edgeList) {
+            //     if (is_piping_op(exp)) {
+            //         apply_op(edge);
+            //     } else {
+            //         throw new Error(
+            //             "Expecting piping operation in connect list"
+            //         );
+            //     }
+            // }
+
+            var graph = new Graph(exp.graph, edges);
+            Graphs[graph_name] = graph;
+            return graph;
+
+        case "spawn_connect":
+            // Can create an unamed graph (no, automatically assign a name) Graph1
+            console.log("spawn_connect cmd not yet supported");
+            return;
+
+        default:
+            /* should never get here */
+            throw new Error("Unsupported command: " + cmd);
+    }
+}
+
+function apply_op(op, left_exp, right_exp) {
+    function num(x) {
+        if (typeof x != "number")
+            throw new Error("Expected number but got " + x);
+        return x;
+    }
+    switch (op) {
+        case "*":
+            var x = evaluate(left_exp);
+            for (var i = 0; i < num(x); i++) {
+                evaluate(right_exp);
+            }
+            return;
+        case "~>":
+        case "->":
+        case "-*>":
+        case "~*>":
+        case "~/>":
+            return new Edge(eval(left_exp), eval(right_exp), op);
+    }
+    throw new Error("Can't apply operator " + op);
+}
+
 var fs = require("fs");
-var input = fs.readFileSync("./input.txt").toString("utf-8");
+var input = fs.readFileSync("./input-test.txt").toString("utf-8");
 
 var inputStream = InputStream(input);
 var tokenStream = TokenStream(inputStream);
@@ -505,9 +754,15 @@ var tokenStream = TokenStream(inputStream);
 // }
 
 var AST = parse(tokenStream);
-console.log("AST:");
-console.log(AST);
-console.log();
+// console.log("AST:");
+// console.log(AST);
+// console.log();
+
+evaluate(AST);
+
+
+// console.log(AST.prog[0].args[0]);
+// console.log(AST.prog[0].args[0].elems);
 
 // console.log(AST.prog[0])
 // console.log(AST.prog[0].args[0].elems)
