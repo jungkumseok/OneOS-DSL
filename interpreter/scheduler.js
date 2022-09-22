4c111e6b7c2c8f242efae6ef215765018c7c093d
Himanshu <himanshu.jaidka@gmail.com>
2022-09-22 15:02:33 -0700

    sample

--- interpreter/scheduler.js
+++ interpreter/scheduler.js
@@ -1,6 +1,54 @@
 const MockRuntime = require("./mock-runtime.js");
 const chalk = require("chalk");
 
+var nodes = [10,20,30,40];
+
+
+class Stack{
+    constructor(){
+        this.items = [];
+        this.size = 50;
+    }
+    push(element){
+        this.items.push(element);
+    }
+    printStack()
+    {
+        var str = "";
+        for (var i = 0; i < this.items.length; i++)
+            str += this.items[i] + " ";
+        return str;
+    }
+}
+
+var stack = new Stack();
+const max = Math.max(...nodes);
+nodes.filter(number => number !== max);
+if(max<=stack.size){
+    stack.push(max);
+    stack.size=stack.size-max;
+}
+// stack.size = fluc;
+if(stack.size>0){
+    for(var i=0; i<nodes.length; i++){
+        if(nodes[i]<=stack.size){
+            stack.push(nodes[i]);
+            stack.size=stack.size-nodes[i];
+        }
+    }   
+}
+// for(var i=0; i<nodes.length; i++){
+//    if(stack.size>=0){
+        
+//         stack.push(nodes[i]);
+//         stack.size = stack.size - nodes[i];
+//     }
+//     // nodes.forEach(element => stack.push(element));
+    
+// } 
+// stack.push(1);
+console.log(stack.printStack());
+console.log(stack.size);
 // getAllRuntimes(){
 //     let info = {};
 //     info[this.config.id] = this.summary();
@@ -9,7 +57,7 @@ const chalk = require("chalk");
 //     });
 //     return info;
 // }
-let runtime = new MockRuntime();
+// let runtime = new MockRuntime();
 // var hosts = [new Host("p1-0")];
 // let info = [];
 // info.push(runtime.hosts);
@@ -17,11 +65,11 @@ let runtime = new MockRuntime();
 // for(let i=0; i<runtime.hosts.length; i++){
 //     count++;
 // }
-let no_of_runtimes = runtime.hosts.length;
-console.log(no_of_runtimes);
-var ou = runtime.hosts;
-console.log(ou);
-console.log(runtime.listProcesses());
+// let no_of_runtimes = runtime.hosts.length;
+// console.log(no_of_runtimes);
+// var ou = runtime.hosts;
+// console.log(ou);
+// console.log(runtime.listProcesses());
 // module.exports = Scheduler;
 
 //sample
